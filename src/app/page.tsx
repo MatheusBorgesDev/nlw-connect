819b7ba5f75b83d7857c4ac6ffd5a5b2d94803f3
MatheusBorgesDev <matheusb.dev@gmail.com>
2025-02-18 01:01:25 -0300

    feat: :sparkles: add Biome and Biome settings

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,28 +1,28 @@
 import { IconButton } from "@/components/icon-button";
-import { Button } from "../components/button";
-import { ArrowRight, Copy, Mail, SendHorizonalIcon } from "lucide-react";
 import { InputField, InputIcon, InputRoot } from "@/components/input";
+import { ArrowRight, Copy, Mail, SendHorizonalIcon } from "lucide-react";
+import { Button } from "../components/button";
 
 export default function Home() {
-  return (
-    <main>
-      <h1 className="text-4xl font-heading">Hello NLW Connect!</h1>
-      <Button type="submit">
-        Enviar <ArrowRight />{" "}
-      </Button>
+	return (
+		<main>
+			<h1 className="text-4xl font-heading">Hello NLW Connect!</h1>
+			<Button type="submit">
+				Enviar <ArrowRight />{" "}
+			</Button>
 
-      <IconButton>
-        <Copy />
-      </IconButton>
+			<IconButton>
+				<Copy />
+			</IconButton>
 
-      <div>
-        <InputRoot error>
-          <InputIcon>
-            <Mail />
-          </InputIcon>
-          <InputField />
-        </InputRoot>
-      </div>
-    </main>
-  );
+			<div>
+				<InputRoot>
+					<InputIcon>
+						<Mail />
+					</InputIcon>
+					<InputField />
+				</InputRoot>
+			</div>
+		</main>
+	);
 }
