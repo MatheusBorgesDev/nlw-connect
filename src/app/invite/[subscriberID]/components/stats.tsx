eebd9596c30e91c04a2c239670910dac685f7713
MatheusBorgesDev <matheusb.dev@gmail.com>
2025-02-22 18:36:17 -0300

    feat: 🎉 get stats data from api

--- src/app/invite/[subscriberID]/components/stats.tsx
+++ src/app/invite/[subscriberID]/components/stats.tsx
@@ -1,13 +1,22 @@
+import {
+	getSubscriberInviteClicks,
+	getSubscriberInviteCount,
+	getSubscriberRankingPosition,
+} from "@/http/api";
 import { BadgeCheck, Medal, MousePointerClick } from "lucide-react";
 import type { ElementType } from "react";
 
 type StatsCardProps = {
-	amountStats: string;
+	amountStats: string | number;
 	description: string;
 	icon: ElementType;
 };
 
-function StatsCard({ amountStats, description, icon: Icon }: StatsCardProps) {
+async function StatsCard({
+	amountStats,
+	description,
+	icon: Icon,
+}: StatsCardProps) {
 	return (
 		<div className="bg-gray-700 border border-gray-600 px-4 py-7 flex flex-col items-center justify-center gap-1 rounded-xl relative">
 			<span className="font-heading text-2xl font-semibold text-gray-200 leading-none">
@@ -22,23 +31,34 @@ function StatsCard({ amountStats, description, icon: Icon }: StatsCardProps) {
 	);
 }
 
-export function Stats() {
+type StatsProps = {
+	subscriberID: string;
+};
+
+export async function Stats({ subscriberID }: StatsProps) {
+	const { count: accessCount } = await getSubscriberInviteClicks(subscriberID);
+	const { count: inviteCount } = await getSubscriberInviteCount(subscriberID);
+	const { position: rankingPosition } =
+		await getSubscriberRankingPosition(subscriberID);
+
+	const isRanked = rankingPosition ? `${rankingPosition}º` : "-";
+
 	return (
 		<div className="grid gap-3 md:grid-cols-3">
 			<StatsCard
-				amountStats="1042"
-				description="Acessos ao link"
+				amountStats={accessCount}
+				description="Cliques no link"
 				icon={MousePointerClick}
 			/>
 
 			<StatsCard
-				amountStats="1042"
+				amountStats={inviteCount}
 				description="Inscrições feitas"
 				icon={BadgeCheck}
 			/>
 
 			<StatsCard
-				amountStats="3º"
+				amountStats={isRanked}
 				description="Posição no ranking"
 				icon={Medal}
 			/>
